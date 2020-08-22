334382b2aa3ed5b2e52d3303880dd3c2019f9a11
deepoil <run-to-run@i.softbank.jp>
2020-08-22 23:02:59 +0900

    first commit

---
+++ bat.go
@@ -0,0 +1,145 @@
+package gobat
+
+import (
+	"errors"
+	"sort"
+	"time"
+)
+
+type CommonBatConfig struct {
+	// The time to start the batch start time
+	StartTime time.Time
+	// Interval to go to check batch start time
+	CheckInterval time.Duration
+	// Interval from start time to start of next batch execution
+	ActionInterval time.Duration
+}
+
+type OneWayBatConfig struct {
+	Common *CommonBatConfig
+}
+
+type ParallelBatConfig struct {
+	Common     *CommonBatConfig
+	Dependency *Dependency
+}
+
+type Dependency struct {
+	bats []*Batch
+}
+
+type Batch struct {
+	priority int
+	function []func()
+}
+
+func SetCommonBatConfig(st time.Time, ci, ac time.Duration) *CommonBatConfig {
+	return &CommonBatConfig{
+		StartTime:      st,
+		CheckInterval:  ci,
+		ActionInterval: ac,
+	}
+}
+
+func SetOneWayBatConfig(common *CommonBatConfig) *OneWayBatConfig {
+	return &OneWayBatConfig{
+		Common: common,
+	}
+}
+
+func SetParallelBatConfig(common *CommonBatConfig, dependency *Dependency) *ParallelBatConfig {
+	return &ParallelBatConfig{
+		Common:     common,
+		Dependency: dependency,
+	}
+}
+
+func SetPriority(p int, fc ...func()) *Batch {
+	return &Batch{
+		priority: p,
+		function: fc,
+	}
+}
+
+func GenerateDependency(ps ...*Batch) (*Dependency, error) {
+	dependency := Dependency{
+		bats: ps,
+	}
+	sort.Sort(dependency)
+
+	// check duplicate
+	var prePriority = -1
+	for _, p := range dependency.bats {
+		if prePriority == p.priority {
+			return nil, errors.New("duplicate priority")
+		}
+		prePriority = p.priority
+	}
+	return &dependency, nil
+}
+
+func (c *CommonBatConfig) NextSchedule() {
+	c.StartTime = c.StartTime.Add(c.ActionInterval)
+}
+
+// Parallel batch processing
+func (b *ParallelBatConfig) ParallelBat() error {
+	if b.Dependency != nil {
+		cs := b.Common
+		for {
+			if time.Now().Equal(cs.StartTime) || time.Now().After(cs.StartTime) {
+				for _, bat := range b.Dependency.bats {
+					var running = make(chan int, len(bat.function))
+					for _, fc := range bat.function {
+						go func(fc func()) {
+							fc()
+							running <- bat.priority
+						}(fc)
+					}
+					for {
+						if len(running) == len(bat.function) {
+							<-running
+							break
+						}
+					}
+				}
+				break
+			}
+			time.Sleep(cs.CheckInterval)
+		}
+		return nil
+	}
+	return errors.New("dependency is not set")
+}
+
+// One-way batch processing
+func (b *OneWayBatConfig) OneWayBat(f ...func()) error {
+	var running = make(chan int, 1)
+	cs := b.Common
+	for {
+		if time.Now().Equal(cs.StartTime) || time.Now().After(cs.StartTime) {
+			for _, fc := range f {
+				go func(fc func()) {
+					fc()
+					running <- 1
+				}(fc)
+				<-running
+			}
+			break
+		}
+		time.Sleep(cs.CheckInterval)
+	}
+	return nil
+}
+
+func (t Dependency) Len() int {
+	return len(t.bats)
+}
+
+func (t Dependency) Swap(i, j int) {
+	t.bats[i], t.bats[j] = t.bats[j], t.bats[i]
+}
+
+func (t Dependency) Less(i, j int) bool {
+	return t.bats[i].priority < t.bats[j].priority
+}
---
+++ examples/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	gobat2 "gobat"
+	"log"
+	"time"
+)
+
+func bat1() {
+	fmt.Println("-----start bat1")
+	time.Sleep(1000 * time.Millisecond)
+	fmt.Println("-----end bat1")
+}
+
+func bat2() {
+	fmt.Println("-----start bat2")
+	time.Sleep(1000 * time.Millisecond)
+	fmt.Println("-----end bat2")
+}
+
+func bat3() {
+	fmt.Println("-----start bat3")
+	time.Sleep(1000 * 10 * time.Millisecond)
+	fmt.Println("-----end bat3")
+}
+
+func bat4() {
+	fmt.Println("-----start bat4")
+	time.Sleep(2000 * time.Millisecond)
+	fmt.Println("-----end bat4")
+}
+
+func bat5() {
+	fmt.Println("-----start bat5")
+	time.Sleep(2000 * time.Millisecond)
+	fmt.Println("-----end bat5")
+}
+
+func bat6() {
+	fmt.Println("-----start bat6")
+	time.Sleep(2000 * time.Millisecond)
+	fmt.Println("-----end bat6")
+}
+
+func bat7() {
+	fmt.Println("-----start bat7")
+	time.Sleep(100 * time.Millisecond)
+	fmt.Println("-----end bat7")
+}
+
+func main() {
+	oneWayCommon := gobat2.SetCommonBatConfig(
+		time.Now(),
+		1000*10*time.Millisecond,
+		1000*60*60*24*time.Millisecond)
+
+	paraCommon := gobat2.SetCommonBatConfig(
+		time.Now(),
+		1000*10*time.Millisecond,
+		1000*60*60*24*time.Millisecond)
+
+	// Prioritize parallel batches
+	p1 := gobat2.SetPriority(1, bat3, bat4, bat1, bat2)
+	p2 := gobat2.SetPriority(2, bat5)
+	p3 := gobat2.SetPriority(3, bat6, bat7)
+
+	// Set the execution order
+	dependency, err := gobat2.GenerateDependency(p1, p2, p3)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Set Config
+	paraBat := gobat2.SetParallelBatConfig(paraCommon, dependency)
+	// one way batch
+	oneWayBat := gobat2.SetOneWayBatConfig(oneWayCommon)
+
+	// Execution example
+	for {
+		if err := paraBat.ParallelBat(); err != nil {
+			log.Fatal("batch error")
+		}
+		paraCommon.NextSchedule()
+		fmt.Printf("next schedule is: %v\n", paraCommon.StartTime)
+
+		if err := oneWayBat.OneWayBat(bat1, bat2, bat3, bat4); err != nil {
+			log.Fatal("batch error")
+		}
+		oneWayCommon.NextSchedule()
+		fmt.Printf("next schedule is: %v\n", oneWayCommon.StartTime)
+	}
+
+}
